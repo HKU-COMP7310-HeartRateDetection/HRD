4123c49202a0a14286230258c729272ce49fef0b
Zheming Kang <zkang018@connect.hku.hk>
2023-12-01 20:28:07 +0800

    Failed to decode
    This version doesn't decode line graph successfully. Please update it.

--- main.py
+++ main.py
@@ -3,22 +3,37 @@ import queue
 import traceback
 import AWSIoTPythonSDK.MQTTLib as AWSIoTPyMQTT
 import PySimpleGUI as sg
+import matplotlib.pyplot as plt
+import matplotlib
 from PIL import Image
+import numpy as np
 import json
+import time
+import base64
+import threading
+from PIL import Image
 
+
+matplotlib.use('TkAgg')
 class Application:
     def __init__(self):
         self.myAWSIoTMQTTClient = None
         self.gui_queue = queue.Queue()
-
+        self.hr_queue = queue.Queue()
+        self.hr_list = []
+        self.hr_lg_queue = queue.Queue()# Line Graph
         middle_font = ('Consolas', 14)
         context_font = ('Consolas', 12)
+        self.plot_timer = None
+        self.current_fr = 30
+        self.plot_lock = threading.Lock()
         sg.theme('DarkGrey14')
-
+        
+        # Column 1: panel 1
         col1 = [[sg.Column([
             [sg.Frame('MQTT Panel 1', [[sg.Column([
                 [sg.Text('Client Id:', font=middle_font)],
-                [sg.Input('Python_Client', key='_CLIENTID_IN_', size=(15, 1), font=context_font),
+                [sg.Input('Sheer_Heart_Attack', key='_CLIENTID_IN_', size=(15, 1), font=context_font),
                  sg.Button('Connect', key='_CONNECT_BTN_', font=context_font)],
                 # Add text boxes and buttons for user input
                 [sg.Text('Frame Size', font=middle_font)],
@@ -36,6 +51,7 @@ class Application:
                 [sg.Multiline(key='_NOTES_', autoscroll=True, size=(20, 10), font=context_font, )],
             ], size=(250, 640), pad=(0, 0))]], font=middle_font)], ], pad=(0, 0), element_justification='c')]]
 
+        # Column 2: panel 2
         col2 = [[sg.Column([
             [sg.Frame('MQTT Panel 2', [[sg.Column([
                 # Add text boxes and buttons for user input
@@ -50,7 +66,7 @@ class Application:
                 # special_effect
                 [sg.Text('Special effects', font=middle_font)],
                 [sg.Combo(['No Effect', 'Negative', 'Grayscale', 'Red Tint', 'Green Tint', 'Blue Tint', 'Sepia'],
-                default_value='Sepia', key='_INPUT6_')],
+                default_value='No Effect', key='_INPUT6_')],
                 [sg.Button('Submit', key='_SUBMIT6_', font=context_font)],
                 # whitebal
                 [sg.Text('whitebalance', font=middle_font), sg.Radio('disable', "RADIO1",key = '_WHIDIS_'), sg.Radio('enable', "RADIO1", key = '_WHIENA_', default=True)],
@@ -65,14 +81,19 @@ class Application:
                 [sg.Button('Submit', key='_SUBMIT9_', font=context_font)],
             ], size=(350, 640), pad=(0, 0))]], font=middle_font)], ], pad=(0, 0), element_justification='c')]]
         
+        # Column 3: 
         col3 = [[sg.Column([
+            # First frame: Camera from mqtt
                 [sg.Frame('CAMERA', [
                 [sg.Image(key='_COMP7310_', size=(480, 320))],
                 ], font=middle_font)],
-                [sg.Text('Heart Rate:', font=middle_font)],
-                [sg.Frame('CAMERA', [
-                [sg.Image(key='_HEARTRAEH_', size=(480, 320))],
-                ], font=middle_font)],
+                # [sg.Frame('Heart Rate', [
+                # [sg.Text(key='_HEARTRATE_')],
+                # ], font=middle_font)],
+            # Second frame: Heart rate from backend
+                [sg.Frame('Heart Rate Line Graph',[
+                [sg.Image(key='_HEARTRATE_', size = (480, 320))],
+                ],)]
                 ], pad=(0, 0), element_justification='c')] 
                 ]
         layout = [[
@@ -84,7 +105,7 @@ class Application:
             event, values = self.window.Read(timeout=5)
             if event is None or event == 'Exit':
                 break
-
+            # Get connection
             if event == '_CONNECT_BTN_':
                 if self.window[event].get_text() == 'Connect':
                     if len(self.window['_CLIENTID_IN_'].get()) == 0:
@@ -113,6 +134,7 @@ class Application:
                 else:
                     input2_value = self.window['_INPUT2_'].get()
                     if input2_value:
+                        self.current_fr = input2_value
                         self.publish_message('config_frame', str(input2_value))
                         self.window['_NOTES_'].print('Set rating frame: ' + str(input2_value))
             # brightness
@@ -184,6 +206,8 @@ class Application:
                     self.publish_message('config_exposure', msg)
                     self.window['_NOTES_'].print('Set exposure ' + str(msg)) 
 
+
+            # Send camera data to gui queue
             try:
                 message = self.gui_queue.get_nowait()
             except queue.Empty:
@@ -192,9 +216,46 @@ class Application:
                 _target_ui = message.get("Target_UI")
                 _image = message.get("Image")
                 self.window[_target_ui].update(data=_image)
+            
+            # Send heart rate data to hr queue
+            try:
+                message = self.hr_queue.get_nowait()
+            except queue.Empty:
+                message = None
+            if message is not None:
+                _target_ui = message.get("Target_UI")
+                _image = message.get("Image")
+                self.window[_target_ui].update(data=_image)
+            
+            # Privious codes: Take the heart rate from mqtt server and update the heart rate text
+            # try: message = self.hr_queue.get_nowait()
+            # except queue.Empty:
+            #     message = None
+            # if message is not None:
+            #     _heartrate = message.get("HEARTRATE")
+            #     _text = message.get("Text")
+            #     if len(self.hr_list)<self.current_fr*60:
+            #         self.hr_list.append(_text)
+            #     else:
+            #         self.hr_list = self.hr_list[1:]  # 删除第一个元素
+            #         self.hr_list.append(_text)  # 在末尾添加新元素
+            #     self.window[_heartrate].update(_text)
+                    
+            # Previous codes: Take the heart rate graph from server and plot
+            # try:
+            #     message = self.hr_lg_queue.get_nowait()
+            # except queue.Empty:
+            #     message = None
+            # if message is not None:
+            #     _target_ui = message.get("Target_UI")
+            #     _image = message.get("Image")
+            #     self.window[_target_ui].update(data=_image)
 
+            # self.plot_hr_graph()
+        # Don't delete this line
         self.window.Close()
 
+# aws connect
     def aws_connect(self, client_id):
         ENDPOINT = "a12ej9mk5jajtb-ats.iot.ap-east-1.amazonaws.com"
         PATH_TO_CERT = "cert.crt"
@@ -209,18 +270,20 @@ class Application:
             if self.myAWSIoTMQTTClient.connect():
                 self.add_note('[MQTT] Connected')
                 self.mqtt_subscribe('COMP7310')
-
+                self.mqtt_subscribe_hr('HEARTRATE')
             else:
                 self.add_note('[MQTT] Cannot Access AWS IOT')
         except Exception as e:
             tb = traceback.format_exc()
             sg.Print(f'An error happened.  Here is the info:', e, tb)
 
+# aws disconnect
     def aws_disconnect(self):
         if self.myAWSIoTMQTTClient is not None:
             self.myAWSIoTMQTTClient.disconnect()
             self.add_note('[MQTT] Successfully Disconnected!')
 
+# subscribe binary stuff from mqtt server, then, put them into gui queue
     def mqtt_subscribe(self, topic):
         if self.myAWSIoTMQTTClient.subscribe(topic, 0, lambda client, userdata, message: {
 
@@ -231,10 +294,38 @@ class Application:
         else:
             self.add_note('[MQTT] Cannot subscribe\nthis Topic: {}'.format(topic))
 
+# another subcribe function which decodes received data with byte64
+    def mqtt_subscribe_hr(self, topic):
+        def handle_message(client, userdata, message):
+            image_data_str = message.payload.decode()  # 将接收到的base64字符串解码为字符串
+            image_data = base64.b64decode(image_data_str)  # 将字符串解码为二进制图像数据
+            image = Image.open(io.BytesIO(image_data))
+            image_bytes = image.tobytes()  # 将图像对象转换为字节数据
+            self.hr_queue.put({"Target_UI": "_{}_".format(str(message.topic).upper()), "Image": image_bytes})
+
+        if self.myAWSIoTMQTTClient.subscribe(topic, 0, handle_message):
+            self.add_note('[MQTT] Topic: {}\n-> Subscribed\n'.format(topic))
+        else:
+            self.add_note('[MQTT] Cannot subscribe\nthis Topic: {}'.format(topic))
+
+# subscribe string data from server, and update the text content. No longer in use
+    def mqtt_subscribe_String(self, topic):
+        if self.myAWSIoTMQTTClient.subscribe(topic, 0, lambda client, userdata, message: {
+
+            self.hr_queue.put({"HEARTRATE": "_{}_".format(str(message.topic).upper()),
+                                "Text": message.payload.decode("utf-8")})
+        }):
+            self.add_note('[MQTT] Topic: {}\n-> Subscribed\n'.format(topic))
+        else:
+            self.add_note('[MQTT] Cannot subscribe\nthis Topic: {}'.format(topic))
+
+
+# Add note to notes
     def add_note(self, note):
         note_history = self.window['_NOTES_'].get()
         self.window['_NOTES_'].update(note_history + note if len(note_history) > 1 else note)
 
+# Convert byte to png file
     def byte_image_to_png(self, message):
         bytes_image = io.BytesIO(message.payload)
         picture = Image.open(bytes_image)
@@ -243,12 +334,45 @@ class Application:
         picture.save(im_bytes, format="PNG")
         return im_bytes.getvalue()
 
+
     def popup_dialog(self, contents, title, font):
         sg.Popup(contents, title=title, keep_on_top=True, font=font)
 
     def publish_message(self, TOPIC, message):
         self.myAWSIoTMQTTClient.publish(TOPIC, str(message), 1) 
         self.window['_NOTES_'].print("Published: '" + str(message) + "' to the topic: " + str(TOPIC))
+
+
+# Plot heart rate graph(line graph). No longer in use
+    def plot_hr_graph(self):
+        if self.window['_CONNECT_BTN_'].get_text() != 'Connect':
+            self.update_plot(self.current_fr)
+            image = Image.open('./heart_rate_line_graph/heart_rate_plot.png')
+            image.thumbnail((480, 320))
+            bytes_io = io.BytesIO()
+            image.save(bytes_io, format='PNG')
+            self.hr_lg_queue.put({"Target_UI": '_HRLG_', "Image": bytes_io.getvalue()})
+
+# Draw the line graph. Update the plot of line graph. No longer in use
+    def update_plot(self, frame_rate):
+        frame_interval = int(frame_rate)  # 每秒取一个点，取整数部分作为帧间隔 30点（帧）分一次秒
+
+        hr_data = self.hr_list  # 获取要绘制的心率数据
+
+        num_points = len(hr_data)  # 数据点的数量
+        # print(num_points)
+        num_seconds = num_points // frame_interval  # 根据帧间隔计算秒数 1800帧/30 = 60秒
+
+        time_values = np.arange(0, num_seconds) * frame_interval
+        hr_values = hr_data[:num_seconds * frame_interval: frame_interval]  # 根据帧间隔获取相应的心率值
+
+        plt.plot(time_values, hr_values)
+        plt.xlabel('Time (seconds)')  # 更新横轴标签为秒
+        plt.ylabel('Heart Rate')
+        plt.title('Heart Rate Variation')
+        plt.grid(True)
+        # plt.tight_layout()
+        plt.savefig('./heart_rate_line_graph/heart_rate_plot.png')
 
 if __name__ == '__main__':
     Application()
